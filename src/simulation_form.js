d6b2272eebad56ef0b39e8b75bbf68359ab61783
Thomas Heslin <tjheslin1@kolabnow.com>
2019-06-16 10:51:21 +0100

    posting all fields

--- src/simulation_form.js
+++ src/simulation_form.js
@@ -7,55 +7,56 @@ export default class SimulationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      simulationCount: 50
+        simulationName: "enter name here",
+        simulationCount: 50,
+        focus: "lowest"
     };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleInputChange(event) {
+  handleChange(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+
+console.log("updating "+ name + " to " + value)
 
     this.setState({
       [name]: value
     });
   }
 
-  handleClick(event) {
-      event.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
 
-      const simulation = {
-        name: this.state.simulationName,
-        focus: this.state.focus
-      };
+    var simulation = {
+      name: this.state.simulationName,
+      simulationCount: this.state.simulationCount,
+      focus: this.state.focus
+    };
 
-      alert('A simulation submitted: ' + simulation);
-
-      axios.post('https://jsonplaceholder.typicode.com', {
-
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: simulation
-      }).then(res => {
-        console.log(res);
-        console.log(res.data);
-      }).catch(error => {
-        console.log(error.config);
-      });
-    }
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+      },
+      body: simulation
+    }).then(res => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.message);
+    });
+  }
 
 // https://stackoverflow.com/questions/38510640/how-to-make-a-rest-post-call-from-reactjs-code
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>Simulation name:</label>
-        <input type="text" name="simulationName" value={this.state.simulationName} />
+        <input type="text" name="simulationName" value={this.state.simulationName} onChange={this.handleChange}  />
         <br />
         <label>
           Number of simulation runs:
@@ -63,7 +64,7 @@ export default class SimulationForm extends React.Component {
             name="simulationCount"
             type="number"
             value={this.state.simulationCount}
-            onChange={this.handleInputChange} />
+            onChange={this.handleChange} />
         </label>
         <br />
         Choose focus strategy (monsters will follow the same strategy):<br />
@@ -72,7 +73,7 @@ export default class SimulationForm extends React.Component {
           <option value="random">Random (inexperienced players)</option>
         </select>
         <br />
-        <input type="submit" value="Submit" onClick={this.handleClick} />
+        <input type="submit" value="Submit" />
       </form>
     );
   }
