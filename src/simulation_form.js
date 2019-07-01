d10caa78d4440aafc8c95752cd03a1d80286439e
Thomas Heslin <tjheslin1@kolabnow.com>
2019-07-01 20:09:11 +0100

    wip

--- src/simulation_form.js
+++ src/simulation_form.js
@@ -33,14 +33,40 @@ export default class SimulationForm extends React.Component {
 
 //    https://stackoverflow.com/questions/9382167/serializing-object-that-contains-cyclic-object-value
 
-    var submittedPlayers = Array.from(document.getElementById("newPlayer").children)
+    var submittedPlayers = Array.from(document.getElementById("players").children).map(playerDiv => {
+      const name = 0
+      const playerClass = 1
+
+      var playerOptions = playerDiv.children
+
+      return {
+        name: playerOptions[name],
+        class: playerOptions[playerClass]
+      };
+    })
+
+    console.log("submittedPlayers = " + submittedPlayers)
+
+    var submittedMonsters = Array.from(document.getElementById("monsters").children).map(monster => monster.value)
 
     var simulation = {
       name: this.state.simulationName,
       simulationCount: this.state.simulationCount,
       focus: this.state.focus,
-      players: submittedPlayers
+      players: submittedPlayers,
+      monsters: submittedMonsters
     };
+
+//    var seen = [];
+//    var json = JSON.stringify(simulation, function(key, val) {
+//       if (val != null && typeof val == "object") {
+//            if (seen.indexOf(val) >= 0) {
+//                return;
+//            }
+//            seen.push(val);
+//        }
+//        return val;
+//    });
 
     axios.post('https://jsonplaceholder.typicode.com/posts', {
       headers: {
@@ -56,24 +82,41 @@ export default class SimulationForm extends React.Component {
   }
 
   appendPlayer(event) {
-    this.appendCreature("newPlayer")
+    this.appendCreature("players")
   }
 
   appendMonster(event) {
-    this.appendCreature("newMonster")
+    this.appendCreature("monsters")
   }
 
   appendCreature(creatureType) {
-    const newCreatureDiv = document.getElementById(creatureType)
+    const creatures = document.getElementById(creatureType)
+    const currentNumCreatures = creatures.children.length
+    const creatureIndex = currentNumCreatures + 1
 
-    const currentNumCreatures = newCreatureDiv.children.length
+    var newCreatureDiv = document.createElement("div")
 
     var input = document.createElement("input");
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', 'enter name');
-    input.setAttribute('name', creatureType + '-' + (currentNumCreatures + 1));
+    input.setAttribute('name', creatureType + '-' + creatureIndex + '-name');
 
     newCreatureDiv.append(input)
+
+    var select = document.createElement("select");
+    select.setAttribute('name', creatureType + '-' + creatureIndex + "-playerClass");
+
+    var playerClasses = "barbarian,cleric,fighter,rogue,wizard"
+    playerClasses.split(',').forEach(function(playerClass) {
+      var option = document.createElement( 'option' );
+      option.setAttribute('value', playerClass)
+      option.innerHTML = playerClass.charAt(0).toUpperCase() + playerClass.substring(1)
+
+      select.append(option)
+    })
+
+    newCreatureDiv.append(select)
+    creatures.append(newCreatureDiv)
   }
 
   render() {
@@ -99,14 +142,25 @@ export default class SimulationForm extends React.Component {
         <br />
         <label>Players:</label>
         <br />
-        <div id="newPlayer">
-            <input type="text" placeholder="enter player name" name="newPlayer-1" />
+        <div id="players">
+            <div id="newPlayer-1">
+              <input type="text" placeholder="enter player name" name="newPlayer-1-name" />
+              <select name="newMonster-1-playerClass">
+                <option defaultValue value= "barbarian">Barbarian</option>
+                <option value= "cleric">Cleric</option>
+                <option value= "fighter">Fighter</option>
+                <option value= "rogue">Rogue</option>
+                <option value= "wizard">Wizard</option>
+              </select>
+            </div>
         </div>
         <button type="button" onClick={this.appendPlayer}>Add Player</button>
         <br />
         <label>Monsters:</label>
-        <div id="newMonster">
-            <input type="text" placeholder="enter player name" name="newMonster-1" />
+        <div id="monsters">
+          <div id="newMonster-1">
+            <input type="text" placeholder="enter player name" name="newMonster-1-name" />
+          </div>
         </div>
         <button type="button" onClick={this.appendMonster}>Add Monster</button>
         <br />
