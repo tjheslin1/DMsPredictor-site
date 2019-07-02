9e6fc0feaaa4f1bca665cd5173b03884983808c4
Thomas Heslin <tjheslin1@kolabnow.com>
2019-07-02 13:43:38 +0100

    Submitting all player info except skills

--- src/simulation_form.js
+++ src/simulation_form.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-// https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-3-daa6b31b66be
-
 export default class SimulationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -31,42 +29,55 @@ export default class SimulationForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-//    https://stackoverflow.com/questions/9382167/serializing-object-that-contains-cyclic-object-value
-
     var submittedPlayers = Array.from(document.getElementById("players").children).map(playerDiv => {
       const name = 0
       const playerClass = 1
+      const level = 2
+      const weapon = 3
+      const str = 4
+      const dex = 5
+      const con = 6
+      const int = 7
+      const wis = 8
+      const cha = 9
 
       var playerOptions = playerDiv.children
 
+      var stats = playerOptions[str].value + "," +
+      playerOptions[dex].value + "," +
+      playerOptions[con].value + "," +
+      playerOptions[int].value + "," +
+      playerOptions[wis].value + "," +
+      playerOptions[cha].value
+
       return {
-        name: playerOptions[name],
-        class: playerOptions[playerClass]
+        name: playerOptions[name].value,
+        class: playerOptions[playerClass].value,
+        level: playerOptions[level].value,
+        weapon: playerOptions[weapon].value,
+        stats: stats
       };
     })
 
-    console.log("submittedPlayers = " + submittedPlayers)
+    var submittedMonsters = Array.from(document.getElementById("monsters").children).map(monsterDiv => {
+      const name = 0
+      const monsterType = 1
 
-    var submittedMonsters = Array.from(document.getElementById("monsters").children).map(monster => monster.value)
+      var monsterOptions = monsterDiv.children
+
+      return {
+        name: monsterOptions[name].value,
+        monster: monsterOptions[monsterType].value
+      };
+    })
 
     var simulation = {
-      name: this.state.simulationName,
-      simulationCount: this.state.simulationCount,
+      simulationName: this.state.simulationName,
+      simulations: this.state.simulationCount,
       focus: this.state.focus,
       players: submittedPlayers,
       monsters: submittedMonsters
     };
-
-//    var seen = [];
-//    var json = JSON.stringify(simulation, function(key, val) {
-//       if (val != null && typeof val == "object") {
-//            if (seen.indexOf(val) >= 0) {
-//                return;
-//            }
-//            seen.push(val);
-//        }
-//        return val;
-//    });
 
     axios.post('https://jsonplaceholder.typicode.com/posts', {
       headers: {
@@ -82,15 +93,11 @@ export default class SimulationForm extends React.Component {
   }
 
   appendPlayer(event) {
-    this.appendCreature("players")
-  }
+    var playerClasses = "barbarian,cleric,fighter,rogue,wizard"
+    var levels = "1,2,3,4,5"
+    var weapons = "shortsword,greatsword,greataxe"
 
-  appendMonster(event) {
-    this.appendCreature("monsters")
-  }
-
-  appendCreature(creatureType) {
-    const creatures = document.getElementById(creatureType)
+    const creatures = document.getElementById("players")
     const currentNumCreatures = creatures.children.length
     const creatureIndex = currentNumCreatures + 1
 
@@ -99,18 +106,87 @@ export default class SimulationForm extends React.Component {
     var input = document.createElement("input");
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', 'enter name');
-    input.setAttribute('name', creatureType + '-' + creatureIndex + '-name');
+    input.setAttribute('name', 'players-' + creatureIndex + '-name');
+
+    newCreatureDiv.append(input)
+
+    var playerClassSelect = document.createElement("select");
+    playerClassSelect.setAttribute('name', 'players-' + creatureIndex + "-playerClass");
+    playerClasses.split(',').forEach(function(playerClass) {
+      var option = document.createElement('option');
+      option.setAttribute('value', playerClass)
+      option.innerHTML = playerClass.charAt(0).toUpperCase() + playerClass.substring(1)
+
+      playerClassSelect.append(option)
+    })
+
+    var levelSelect = document.createElement("select");
+    levelSelect.setAttribute('name', 'players-' + creatureIndex + "-level");
+    levels.split(',').forEach(function(level) {
+      var option = document.createElement('option');
+      option.setAttribute('value', level)
+      option.innerHTML = "Level " + level
+
+      levelSelect.append(option)
+    })
+
+    var weaponSelect = document.createElement("select");
+    weaponSelect.setAttribute('name', 'players-' + creatureIndex + "-weapon");
+    weapons.split(',').forEach(function(weapon) {
+      var option = document.createElement('option');
+      option.setAttribute('value', weapon)
+      option.innerHTML = weapon.charAt(0).toUpperCase() + weapon.substring(1)
+
+      weaponSelect.append(option)
+    })
+
+    newCreatureDiv.append(playerClassSelect)
+    newCreatureDiv.append(levelSelect)
+    newCreatureDiv.append(weaponSelect)
+
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Str"))
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Dex"))
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Con"))
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Int"))
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Wis"))
+    newCreatureDiv.append(this.appendModScore(creatureIndex, "Cha"))
+
+    creatures.append(newCreatureDiv)
+  }
+
+  appendModScore(creatureIndex, score) {
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('class', 'mod-score');
+    input.setAttribute('placeholder', score.charAt(0).toUpperCase() + score.substring(1));
+    input.setAttribute('name', 'players-' + creatureIndex + '-' + score);
+
+    return input
+  }
+
+  appendMonster(event) {
+    var monsterTypes = "goblin,zombie,vampire,werewolf"
+
+    const creatures = document.getElementById("monsters")
+    const currentNumCreatures = creatures.children.length
+    const creatureIndex = currentNumCreatures + 1
+
+    var newCreatureDiv = document.createElement("div")
+
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', 'enter name');
+    input.setAttribute('name', 'monsters-' + creatureIndex + '-name');
 
     newCreatureDiv.append(input)
 
     var select = document.createElement("select");
-    select.setAttribute('name', creatureType + '-' + creatureIndex + "-playerClass");
+    select.setAttribute('name', 'monsters-' + creatureIndex + "-playerClass");
 
-    var playerClasses = "barbarian,cleric,fighter,rogue,wizard"
-    playerClasses.split(',').forEach(function(playerClass) {
+    monsterTypes.split(',').forEach(function(monster) {
       var option = document.createElement( 'option' );
-      option.setAttribute('value', playerClass)
-      option.innerHTML = playerClass.charAt(0).toUpperCase() + playerClass.substring(1)
+      option.setAttribute('value', monster)
+      option.innerHTML = monster.charAt(0).toUpperCase() + monster.substring(1)
 
       select.append(option)
     })
@@ -143,23 +219,47 @@ export default class SimulationForm extends React.Component {
         <label>Players:</label>
         <br />
         <div id="players">
-            <div id="newPlayer-1">
-              <input type="text" placeholder="enter player name" name="newPlayer-1-name" />
-              <select name="newMonster-1-playerClass">
-                <option defaultValue value= "barbarian">Barbarian</option>
-                <option value= "cleric">Cleric</option>
-                <option value= "fighter">Fighter</option>
-                <option value= "rogue">Rogue</option>
-                <option value= "wizard">Wizard</option>
-              </select>
-            </div>
+          <div id="players-1">
+            <input type="text" placeholder="enter player name" name="players-1-name" />
+            <select name="players-1-playerClass">
+              <option defaultValue value="barbarian">Barbarian</option>
+              <option value= "cleric">Cleric</option>
+              <option value= "fighter">Fighter</option>
+              <option value= "rogue">Rogue</option>
+              <option value= "wizard">Wizard</option>
+            </select>
+            <select name="players-1-level">
+              <option defaultValue value="1">Level 1</option>
+              <option value= "2">Level 2</option>
+              <option value= "3">Level 3</option>
+              <option value= "4">Level 4</option>
+              <option value= "5">Level 5</option>
+            </select>
+            <select name="players-1-weapon">
+              <option defaultValue value="shortsword">Shortsword</option>
+              <option value= "greatsword">Greatsword</option>
+              <option value= "greataxe">Greataxe</option>
+            </select>
+            <input type="text" class="mod-score" placeholder="Str" name="players-1-str" />
+            <input type="text" class="mod-score" placeholder="Dex" name="players-1-dex" />
+            <input type="text" class="mod-score" placeholder="Con" name="players-1-con" />
+            <input type="text" class="mod-score" placeholder="Int" name="players-1-int" />
+            <input type="text" class="mod-score" placeholder="Wis" name="players-1-wis" />
+            <input type="text" class="mod-score" placeholder="Cha" name="players-1-cha" />
+          </div>
         </div>
         <button type="button" onClick={this.appendPlayer}>Add Player</button>
         <br />
         <label>Monsters:</label>
         <div id="monsters">
-          <div id="newMonster-1">
-            <input type="text" placeholder="enter player name" name="newMonster-1-name" />
+          <div id="monsters-1">
+            <input type="text" placeholder="enter player name" name="monsters-1-name" />
+            <select name="monsters-1-monsterType">
+              <option defaultValue value="goblin">Goblin</option>
+              <option value= "zombie">Zombie</option>
+              <option value= "vampire">Vampire</option>
+              <option value= "werewolf">Werewolf</option>
+            </select>
           </div>
         </div>
         <button type="button" onClick={this.appendMonster}>Add Monster</button>
