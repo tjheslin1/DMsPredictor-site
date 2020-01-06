ce2bf142dc60c1603f75c4ed342e2351784dbbc3
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2020-01-06 15:15:38 +0000

    Updates

---
+++ precache-manifest.1b39d6ec8fac2ed57aac6366091ad2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21d5604728ce8f4d38267b09385ba1a",
+    "url": "/DMsPredictor-site/index.html"
+  },
+  {
+    "revision": "c23391ab2ac1351748de",
+    "url": "/DMsPredictor-site/static/css/main.4df2dadd.chunk.css"
+  },
+  {
+    "revision": "0d286fe8f69d2f448023",
+    "url": "/DMsPredictor-site/static/js/2.6706b9e2.chunk.js"
+  },
+  {
+    "revision": "664aead790f3c2ec75f52b99d39b9eb0",
+    "url": "/DMsPredictor-site/static/js/2.6706b9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c23391ab2ac1351748de",
+    "url": "/DMsPredictor-site/static/js/main.451ecc5a.chunk.js"
+  },
+  {
+    "revision": "2c64025c054ab5727430",
+    "url": "/DMsPredictor-site/static/js/runtime-main.88105319.js"
+  }
+]);
